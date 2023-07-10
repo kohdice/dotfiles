d1ca229845ff4db965f35b699fe58851eb8b75e2
kohdice <kohdice@gmail.com>
2023-07-11 00:55:58 +0900

    scripts: Remove unnecessary code

--- scripts/macos/dotfiles.sh
+++ scripts/macos/dotfiles.sh
@@ -4,9 +4,8 @@ echo "[dotfiles.sh] Copy configuration files..."
 
 DOTFILES="${HOME}/dotfiles"
 ZSH="${DOTFILES}/zsh"
-TMUX="${DOTFILES}/tmux"
 
-cp -fR "${DOTFILES}/.config" "${DOTFILES}/.gitconfig" "${ZSH}/.zshrc" "${ZSH}/.p10k.zsh" "${TMUX}/.tmux.conf" "${TMUX}/.tmux.powerline.conf" ${HOME}
+cp -fR "${DOTFILES}/.config" "${DOTFILES}/.gitconfig" "${ZSH}/.zshrc" "${ZSH}/.p10k.zsh" ${HOME}
 
 echo "=> Done."
 echo

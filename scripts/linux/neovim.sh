304f01a3c55a19518f05f713d08b82cc9c7d5c51
kohdice <kohdice@gmail.com>
2023-07-20 23:00:00 +0900

    scripts: Refactoring

--- scripts/linux/neovim.sh
+++ scripts/linux/neovim.sh
@@ -2,10 +2,8 @@
 
 echo "[neovim.sh] Install Neovim..."
 
-cd ~
+cd $HOME
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 make install
-
-echo "=> Done"
 echo
--- scripts/linux/tools.sh
+++ scripts/linux/tools.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
-echo "[tools.sh] Install tools..."
-echo
-
 cd ~
 
 # git tools for bash
-echo "[git-completion,prompt] Install..."
+echo "[tools.sh] Install git-completion,prompt..."
 curl -o .git-completion.sh \
     https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o .git-prompt.sh \
     https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-echo "=> Done"
 echo
 
 # Fisher
-echo "[Fisher] Install..."
+echo "[[tools.sh] Install Fisher..."
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-echo "=> Done"
 echo
 
 # act
-echo "[act] Install..."
+echo "[tools.sh] Install act..."
 curl -s https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
-echo "=> Done"
 echo
 
 # Lazygit
-echo "[Lazygit] Install..."
+echo "[tools.sh] Install Lazygit..."
 
 mkdir ${HOME}/tools
 cd ${HOME}/tools
@@ -36,5 +30,4 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 install lazygit /usr/local/bin
-echo "=> Done"
 echo

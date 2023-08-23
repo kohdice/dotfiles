c25fc7442cdc85d6677a02aec3d413b9d4287c39
kohdice <kohdice@gmail.com>
2023-08-23 21:37:08 +0900

    nvim: Change to nvimdev/lspsaga.nvim
    Include adding shortcuts.

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -1,5 +1,5 @@
 return {
-  'glepnir/lspsaga.nvim',
+  'nvimdev/lspsaga.nvim',
   dependencies = {
     { 'nvim-tree/nvim-web-devicons' },
     --Please make sure you install markdown and markdown_inline parser
@@ -9,11 +9,16 @@ return {
   config = function()
     require('lspsaga').setup({
       ui = {
-        winblend = 10,
         border = 'rounded',
-        colors = {
-          normal_bg = '#002b36'
-        }
+      },
+      symbol_in_winbar = {
+        enable = false
+      },
+      lightbulb = {
+        enable = false
+      },
+      outline = {
+        layout = 'float'
       }
     })
 
@@ -22,7 +27,8 @@ return {
     vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
     vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<CR>', opts)
     vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-    vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+    vim.keymap.set('n', 'gd', '<Cmd>Lspsaga finder<CR>', opts)
+    vim.keymap.set('n', 'gtt', '<Cmd>Lspsaga goto_type_definition<CR>', opts)
     -- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
     vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)

ef2e69ccfc308e3e4cfc93529fd2c693a513c225
kohdice <kohdice@gmail.com>
2023-11-13 13:27:07 +0900

    nvim: Add config for displaying diff in lualine
    Includes update config for branch icons and theme settings.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,19 +5,31 @@ return {
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'onedark',
+        theme = 'auto',
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
         disabled_filetypes = {}
       },
     sections = {
       lualine_a = { 'mode' },
-      lualine_b = { 'branch' },
-      lualine_c = { {
-        'filename',
-        file_status = true, -- displays file status (readonly status, modified status)
-        path = 0            -- 0 = just filename, 1 = relative path, 2 = absolute path
-      } },
+      lualine_b = {
+        { 'branch', icon = '' }
+      },
+      lualine_c = {
+        {
+          'filename',
+          file_status = true, -- displays file status (readonly status, modified status)
+          path = 0            -- 0 = just filename, 1 = relative path, 2 = absolute path
+        },
+        {
+          'diff',
+          symbols = {
+            added = ' ',
+            modified = ' ',
+            removed = ' '
+          },
+        }
+      },
       lualine_x = {
         {
           'diagnostics',

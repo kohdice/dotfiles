f53efdd0f2c0b7bdcad78fbc5a537c936b6cda62
kohdice <kohdice@gmail.com>
2024-04-28 23:13:12 +0900

    fish: Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,45 +15,49 @@ set -g hydro_multiline true
 
 
 ## aliases
+alias cl clear
+
 alias ls "ls -p -G"
 alias la "ls -A"
 alias ll "ls -l"
 alias lla "ll -A"
 
-# eza
-if type -q eza
-  alias ll "eza -l -g --icons"
-  alias lla "ll -a"
-end
-
 alias ..2 "cd ../.."
 alias ..3 "cd ../../.."
 alias ..4 "cd ../../../../"
 
-alias cl clear
+# eza
+if type -q eza
+    alias ll "eza -l -g --icons"
+    alias lla "ll -a"
+end
 
-#git
+# git
 alias g git
-# lazygit
-alias lg lazygit
 
-# docker
-alias d "docker"
-alias dc "docker-compose"
-alias dcu "docker-compose up"
-alias dcub "docker-compose up --build"
-alias dcud "docker-compose up -d"
-alias dcudb "docker-compose up -d --build"
-alias dce "docker-compose exec"
+# fish
+alias fishconfig "nvim  ~/.config/fish/config.fish"
+alias fishreload "source  ~/.config/fish/config.fish"
 
 # Neovim
 command -qv nvim && alias vim nvim
 alias v nvim
 alias view "nvim -R"
 
-# fish
-alias fishconfig "nvim  ~/.config/fish/config.fish"
-alias fishreload "source  ~/.config/fish/config.fish"
+# docker
+alias d docker
+alias dc docker-compose
+alias dcu "docker-compose up"
+alias dcub "docker-compose up --build"
+alias dcud "docker-compose up -d"
+alias dcudb "docker-compose up -d --build"
+alias dce "docker-compose exec"
+
+# Terraform
+alias tf terraform
+
+# lazygit
+alias lg lazygit
 
 
 ## Path
@@ -76,3 +80,7 @@ pyenv init - | source
 # volta
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# fzf
+set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
+set -g FZF_LEGACY_KEYBINDINGS 0

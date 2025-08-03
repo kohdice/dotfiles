db8f46618cf685fce97d0d83667023dad4c7273b
kohdice <kohdice@gmail.com>
2025-08-03 17:18:37 +0900

    chore: Remove unnecessary files

--- scripts/linux/neovim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "[neovim.sh] Install Neovim..."
-
-cd $HOME
-git clone https://github.com/neovim/neovim
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-make install
-echo
--- scripts/macos/dotfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "[dotfiles.sh] Copy dotfiles..."
-
-DOTFILES="${HOME}/dotfiles"
-ZSH="${DOTFILES}/zsh"
-
-cp -fR "${DOTFILES}/.config" "${DOTFILES}/.gitconfig" "${ZSH}/.zshrc" "${ZSH}/.p10k.zsh" ${HOME}
-echo

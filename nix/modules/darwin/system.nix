ee3f977e7ea38659eb795bac9809ed86b3b7f984
kohdice <kohdice@gmail.com>
2025-12-28 23:16:10 +0900

    refactor(darwin): Remove unnecessary system settings

--- nix/modules/darwin/system.nix
+++ nix/modules/darwin/system.nix
@@ -1,54 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  # macOS system settings
   system.defaults = {
-    # Dock settings
     dock = {
       autohide = true;
-      show-recents = false;
-      tilesize = 48;
-      mru-spaces = false;
+      tilesize = 36;
     };
 
-    # Finder settings
     finder = {
-      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
       ShowPathbar = true;
-      ShowStatusBar = true;
-      FXEnableExtensionChangeWarning = false;
-      FXPreferredViewStyle = "Nlsv"; # List view
+      ShowStatusBar = false;
     };
 
-    # Keyboard settings
     NSGlobalDomain = {
-      AppleKeyboardUIMode = 3;
-      ApplePressAndHoldEnabled = false;
-      KeyRepeat = 2;
-      InitialKeyRepeat = 15;
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticSpellingCorrectionEnabled = false;
-    };
-
-    # Trackpad settings
-    trackpad = {
-      Clicking = true;
-      TrackpadThreeFingerDrag = true;
-    };
-
-    # Login window
-    loginwindow = {
-      GuestEnabled = false;
+      KeyRepeat = 1;
+      InitialKeyRepeat = 10;
     };
   };
 
-  # Security settings
   security.pam.enableSudoTouchIdAuth = true;
 
-  # Fonts
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
   ];

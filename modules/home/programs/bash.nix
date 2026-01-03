94ea262aa17710047cc02f6727f679e010049d47
kohdice <kohdice@gmail.com>
2026-01-04 00:08:37 +0900

    refactor(shell): Modularize shell configuration

---
+++ modules/home/programs/bash.nix
@@ -0,0 +1,72 @@
+{ ... }:
+
+let
+  aliases = import ../shell/aliases.nix;
+  env = import ../shell/env.nix;
+  paths = import ../shell/paths.nix;
+in
+{
+  programs.bash = {
+    enable = true;
+    shellAliases = aliases // {
+      bashreload = "source ~/.bashrc";
+    };
+    sessionVariables = env;
+
+    historySize = 10000;
+    historyFileSize = 10000;
+    historyControl = [
+      "ignoredups"
+      "erasedups"
+    ];
+
+    shellOptions = [
+      "histappend"
+      "checkwinsize"
+      "extglob"
+      "globstar"
+      "checkjobs"
+    ];
+
+    initExtra = ''
+      # Starship
+      eval "$(starship init bash)"
+
+      # zoxide
+      eval "$(zoxide init bash)"
+
+      ### fzf ###
+
+      # fzf history search (Ctrl+R)
+      fzf-history() {
+        local selected
+        selected=$(history | tac | awk '{$1=""; print substr($0,2)}' | fzf --query "$READLINE_LINE" --reverse)
+        READLINE_LINE="$selected"
+        READLINE_POINT=''${#READLINE_LINE}
+      }
+      bind -x '"\C-r": fzf-history'
+
+      # fzf ghq (Ctrl+T)
+      ghq-fzf() {
+        local src
+        src=$(ghq list | fzf --preview "bat --color=always --style=grid $(ghq root)/{}/README.*")
+        if [ -n "$src" ]; then
+          cd "$(ghq root)/$src" || return
+        fi
+      }
+      bind -x '"\C-t": ghq-fzf'
+
+      # fzf cd to recent directory (Ctrl+F)
+      fzf-zoxide() {
+        local selected
+        selected=$(zoxide query -l | fzf --reverse)
+        if [ -n "$selected" ]; then
+          cd "$selected" || return
+        fi
+      }
+      bind -x '"\C-f": fzf-zoxide'
+    '';
+  };
+
+  home.sessionPath = paths;
+}
--- modules/home/programs/zsh.nix
+++ modules/home/programs/zsh.nix
@@ -1,12 +1,87 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
+let
+  aliases = import ../shell/aliases.nix;
+  env = import ../shell/env.nix;
+  paths = import ../shell/paths.nix;
+in
 {
-  # Disable programs.zsh to use dotfiles symlink instead
-  programs.zsh.enable = false;
+  programs.zsh = {
+    enable = true;
+    shellAliases = aliases // {
+      zshreload = "source ~/.zshrc";
+    };
+    sessionVariables = env;
 
-  # Install zsh plugins as packages
-  home.packages = with pkgs; [
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-  ];
+    # Zsh options
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    history = {
+      size = 10000;
+      save = 10000;
+      ignoreDups = true;
+      share = true;
+    };
+
+    initExtra = ''
+      # Zsh options
+      setopt no_beep
+      setopt auto_pushd
+      setopt pushd_ignore_dups
+      setopt inc_append_history
+
+      # Starship
+      eval "$(starship init zsh)"
+
+      # zoxide
+      eval "$(zoxide init zsh)"
+
+      ### fzf ###
+
+      # fzf history
+      fzf-select-history() {
+        BUFFER=$(history -n -r 1 | fzf --query "$LBUFFER" --reverse)
+        CURSOR=$#BUFFER
+        zle reset-prompt
+      }
+      zle -N fzf-select-history
+      bindkey '^r' fzf-select-history
+
+      # cdr setup
+      if [[ -n $(echo ''${^fpath}/chpwd_recent_dirs(N)) && -n $(echo ''${^fpath}/cdr(N)) ]]; then
+        autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+        add-zsh-hook chpwd chpwd_recent_dirs
+        zstyle ':completion:*' recent-dirs-insert both
+        zstyle ':chpwd:*' recent-dirs-default true
+        zstyle ':chpwd:*' recent-dirs-max 1000
+      fi
+
+      # fzf cdr
+      fzf-cdr() {
+        local selected_dir=$(cdr -l | awk '{ print $2 }' | fzf --reverse)
+        if [ -n "$selected_dir" ]; then
+          BUFFER="cd ''${selected_dir}"
+          zle accept-line
+        fi
+        zle clear-screen
+      }
+      zle -N fzf-cdr
+      bindkey '^f' fzf-cdr
+
+      # fzf ghq
+      ghq-fzf() {
+        local src=$(ghq list | fzf --preview "bat --color=always --style=grid $(ghq root)/{}/README.*")
+        if [ -n "$src" ]; then
+          BUFFER="cd $(ghq root)/$src"
+          zle accept-line
+        fi
+        zle -R -c
+      }
+      zle -N ghq-fzf
+      bindkey '^t' ghq-fzf
+    '';
+  };
+
+  home.sessionPath = paths;
 }

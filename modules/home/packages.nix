57f45503ecb7472a5557cc5d1c40b014cdadc710
kohdice <kohdice@gmail.com>
2026-01-04 10:34:52 +0900

    refactor(packages): Reorganize GUI apps between Nix and Homebrew

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -3,6 +3,7 @@
 let
   isDarwin = pkgs.stdenv.isDarwin;
   isLinux = pkgs.stdenv.isLinux;
+  isX86_64Linux = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
 in
 {
   home.packages =
@@ -11,6 +12,7 @@ in
       # Core tools
       bat
       curl
+      dust
       eza
       fd
       fzf
@@ -26,6 +28,7 @@ in
 
       # Terminal tools
       fastfetch
+      navi
       starship
       yazi
       zoxide
@@ -41,13 +44,20 @@ in
       google-chrome
     ]
     ++ lib.optionals isDarwin [
+      # GUI Apps
+      slack
       tableplus
-      zoom-us
+      vscode
     ]
     ++ lib.optionals isLinux [
+      # Development tools
       docker
+
+      # GUI Apps
       ghostty
-      mysql-workbench
-      zoom-us
+    ]
+    ++ lib.optionals (isDarwin || isX86_64Linux) [
+      # Discord (aarch64-linux not supported)
+      discord
     ];
 }

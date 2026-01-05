0521ad0de9f1ff0d0df82e293d68c60c9ae1d478
kohdice <kohdice@gmail.com>
2026-01-05 17:44:05 +0900

    refactor(mkSystem): Derive home and dotfilesDir from username

--- users/kohdice/info.nix
+++ users/kohdice/info.nix
@@ -3,6 +3,4 @@
   name = "kohdice";
   fullName = "kohdice";
   email = "kohdice@gmail.com";
-  home = "/Users/kohdice";
-  dotfilesDir = "/Users/kohdice/developments/dotfiles";
 }
--- users/work/info.nix
+++ users/work/info.nix
@@ -3,6 +3,4 @@
   name = "karei";
   fullName = "kohdice";
   email = "kohdice@gmail.com";
-  home = "/Users/karei";
-  dotfilesDir = "/Users/karei/kohdice/dotfiles";
 }

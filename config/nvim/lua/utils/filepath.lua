992dcd29deba8be4e75aeebb9bd0128f41ec7ae9
kohdice <k@kohdice.com>
2026-06-12 12:15:38 +0900

    refactor(nvim): simplify filepath helpers

--- config/nvim/lua/utils/filepath.lua
+++ config/nvim/lua/utils/filepath.lua
@@ -1,57 +1,42 @@
 local M = {}
 
+local function copy_to_clipboard(text)
+  vim.fn.setreg("+", text)
+  vim.notify('Copied: "' .. text .. '"')
+end
+
+local function get_line_info()
+  -- "\22" is visual-block mode (CTRL-V)
+  if vim.fn.mode():match("[vV\22]") then
+    -- '< / '> marks are only updated after leaving visual mode,
+    -- so use the cursor and the other end of the active selection
+    local start_line = vim.fn.line(".")
+    local end_line = vim.fn.line("v")
+    if start_line > end_line then
+      start_line, end_line = end_line, start_line
+    end
+    if start_line == end_line then
+      return "#L" .. start_line
+    end
+    return "#L" .. start_line .. "-L" .. end_line
+  end
+  return "#L" .. vim.fn.line(".")
+end
+
 M.copy_absolute_path = function()
-  local path = vim.fn.expand("%:p")
-  vim.fn.setreg("+", path)
-  vim.notify('Copied: "' .. path .. '"')
+  copy_to_clipboard(vim.fn.expand("%:p"))
 end
 
 M.copy_absolute_path_with_line = function()
-  local path = vim.fn.expand("%:p")
-  local line_info = ""
-
-  if vim.fn.mode():match("[vV]") then
-    local start_line = vim.fn.line("'<")
-    local end_line = vim.fn.line("'>")
-    if start_line == end_line then
-      line_info = "#L" .. start_line
-    else
-      line_info = "#L" .. start_line .. "-L" .. end_line
-    end
-  else
-    line_info = "#L" .. vim.fn.line(".")
-  end
-
-  local result = path .. line_info
-  vim.fn.setreg("+", result)
-  vim.notify('Copied: "' .. result .. '"')
+  copy_to_clipboard(vim.fn.expand("%:p") .. get_line_info())
 end
 
 M.copy_relative_path = function()
-  local path = vim.fn.expand("%")
-  vim.fn.setreg("+", path)
-  vim.notify('Copied: "' .. path .. '"')
+  copy_to_clipboard(vim.fn.expand("%"))
 end
 
 M.copy_relative_path_with_line = function()
-  local path = vim.fn.expand("%")
-  local line_info = ""
-
-  if vim.fn.mode():match("[vV]") then
-    local start_line = vim.fn.line("'<")
-    local end_line = vim.fn.line("'>")
-    if start_line == end_line then
-      line_info = "#L" .. start_line
-    else
-      line_info = "#L" .. start_line .. "-L" .. end_line
-    end
-  else
-    line_info = "#L" .. vim.fn.line(".")
-  end
-
-  local result = path .. line_info
-  vim.fn.setreg("+", result)
-  vim.notify('Copied: "' .. result .. '"')
+  copy_to_clipboard(vim.fn.expand("%") .. get_line_info())
 end
 
 return M

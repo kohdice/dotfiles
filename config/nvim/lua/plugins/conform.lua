13bf6cea45866231e5ca5af8f462eb8030c074b2
kohdice <k@kohdice.com>
2026-09-15 17:12:01 +0900

    fix(nvim): Use clang-format instead of clangd for Protobuf

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -33,7 +33,7 @@ return {
           lua = { "stylua" },
           markdown = { "prettierd", "prettier", stop_after_first = true },
           objcpp = { lsp_format = "prefer" },
-          proto = { lsp_format = "prefer" },
+          proto = { "clang_format" },
           python = function(bufnr)
             if require("conform").get_formatter_info("ruff_format", bufnr).available then
               return { "ruff_format" }
